cf2c4a487a8b08008fbd4cf3785071b8c014363c
Maros <apmaros@gmail.com>
2020-04-26 13:29:00 +0100

    feat(logging): add time to logs

--- wetogether/app.py
+++ wetogether/app.py
@@ -8,7 +8,7 @@ from db.session import get_session
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-
+logging.basicConfig(format='%(asctime)-15s %(message)s')
 
 def run():
     logger.info("starting Together API")
