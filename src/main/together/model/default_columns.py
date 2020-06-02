4945a2202d78c8be77c7f1e212280bb4a86afd9c
Maros <apmaros@gmail.com>
2020-06-02 21:49:31 +0100

    fix: generator fn insted of global shared constant

--- src/main/together/model/default_columns.py
+++ src/main/together/model/default_columns.py
@@ -3,14 +3,16 @@ from sqlalchemy import Column, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 
 id_uuid = Column(
-        UUID(as_uuid=True),
-        primary_key=True,
-        default=uuid.uuid4,
-        unique=True,
-        nullable=False,
-    )
+    UUID(as_uuid=True),
+    primary_key=True,
+    default=uuid.uuid4,
+    unique=True,
+    nullable=False,
+)
 
-user_id_foreign_key = Column(
+
+def make_user_id_fk() -> Column:
+    return Column(
         ForeignKey('users.id'),
         UUID(as_uuid=True),
         primary_key=True,
--- src/main/together/model/post.py
+++ src/main/together/model/post.py
@@ -3,12 +3,12 @@ from tokenize import String
 from sqlalchemy import Column
 
 from model.base import Base
-from model.default_columns import user_id_foreign_key
+from model.default_columns import make_user_id_fk
 
 
 class Post(Base):
     __tablename__ = "posts"
 
-    user_id = user_id_foreign_key
+    user_id = make_user_id_fk()
     body = Column(String)
     url = Column(String)
